465d0c13080d812a85f1278186288b2a13924b3c
m-pawlowicz <michal.pawlowicz.dev@gmail.com>
2025-05-27 10:30:43 +0200

    add starter code for nginx configurations section

---
+++ nginx_configurations/src/index.js
@@ -0,0 +1,17 @@
+// Standard Express boilerplate
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
